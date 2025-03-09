fa26028f0ecacf111947c76a37af4ea6b29e8a82
Solemarc <larco9mason@gmail.com>
2025-03-09 15:49:35 +1000

    add scenes folder

--- scenes/main.gd
+++ scenes/main.gd
@@ -8,7 +8,6 @@ var node: Node
 var board_width: int
 var board_height: int
 var grid_data: Array
-var winner: String
 const grid_pins: Array = [
 	[Vector2(111.0, 90), Vector2(450.0, 88.0), Vector2(775, 95.0)],
 	[Vector2(108.0, 305.0), Vector2(441.0, 306.0), Vector2(773.0, 301.0)],
@@ -21,7 +20,7 @@ func _ready() -> void:
 	new_game()
 
 func _input(event: InputEvent) -> void:
-	if event is InputEventMouseButton && winner == "":
+	if event is InputEventMouseButton && $WinnerLabel.text == "":
 		if event.button_index == MOUSE_BUTTON_LEFT && event.pressed && event.position[0] <= 900:
 			var pos = Vector2i(get_column(event.position[0]), get_row(event.position[1]))
 			if grid_data[pos.y-1][pos.x-1] == 0:
@@ -29,8 +28,8 @@ func _input(event: InputEvent) -> void:
 				node.queue_free()
 				create_marker(pos)
 				create_marker(pos, true)
-				player *= -1
 				validate_board()
+				player *= -1
 
 # Covert mouse position to column number
 func get_column(c: int) -> int:
@@ -57,22 +56,13 @@ func new_game() -> void:
 		[0, 0, 0],
 	]
 	create_marker(Vector2i(0, 0), true)
-	winner = ""
 
 func create_marker(pos: Vector2i, temp: bool = false) -> void:
 	#create a marker node and add as child
 	const panel_pos = Vector2i(1051, 175)
 	const scale = Vector2(0.2, 0.2)
 	var position = grid_pins[pos.y-1][pos.x-1]
-	if player == -1:
-		node = circle_scene.instantiate()
-		node.scale = scale
-		if temp:
-			node.position = panel_pos
-		else:
-			node.position = position
-		add_child(node)
-	else:
+	if player == 1:
 		node = cross_scene.instantiate()
 		node.scale = scale
 		if temp:
@@ -80,10 +70,18 @@ func create_marker(pos: Vector2i, temp: bool = false) -> void:
 		else:
 			node.position = position
 		add_child(node)
+	else:
+		node = circle_scene.instantiate()
+		node.scale = scale
+		if temp:
+			node.position = panel_pos
+		else:
+			node.position = position
+		add_child(node)
 
-# cross = 1, circle = -1
 func validate_board() -> void:
 	# check rows for a win
+	var winner = ""
 	if abs(grid_data[0][0] + grid_data[0][1] + grid_data[0][2]) == 3:
 		winner = get_winner(grid_data[0][0])
 	if abs(grid_data[1][0] + grid_data[1][1] + grid_data[1][2]) == 3:
