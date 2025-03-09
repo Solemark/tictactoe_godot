d0fc840686be267d5d1dd4e63526b1ffd2895586
Solemarc <larco9mason@gmail.com>
2025-03-09 16:34:10 +1000

    complete tictactoe

--- scripts/main.gd
+++ scripts/main.gd
@@ -5,9 +5,8 @@ extends Node
 
 var player: int
 var node: Node
-var board_width: int
-var board_height: int
 var grid_data: Array
+var turn: int
 const grid_pins: Array = [
 	[Vector2(111.0, 90), Vector2(450.0, 88.0), Vector2(775, 95.0)],
 	[Vector2(108.0, 305.0), Vector2(441.0, 306.0), Vector2(773.0, 301.0)],
@@ -15,12 +14,10 @@ const grid_pins: Array = [
 ]
 
 func _ready() -> void:
-	board_width = $Board.texture.get_width()
-	board_height = $Board.texture.get_height()
 	new_game()
 
 func _input(event: InputEvent) -> void:
-	if event is InputEventMouseButton && $WinnerLabel.text == "":
+	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT && event.pressed && event.position[0] <= 900:
 			var pos = Vector2i(get_column(event.position[0]), get_row(event.position[1]))
 			if grid_data[pos.y-1][pos.x-1] == 0:
@@ -28,8 +25,11 @@ func _input(event: InputEvent) -> void:
 				node.queue_free()
 				create_marker(pos)
 				create_marker(pos, true)
-				validate_board()
+				if validate_board():
+					get_tree().paused = true
+					$GameOverMenu.show()
 				player *= -1
+				turn += 1
 
 # Covert mouse position to column number
 func get_column(c: int) -> int:
@@ -49,63 +49,90 @@ func get_row(r: int) -> int:
 		return 3
 
 func new_game() -> void:
-	player = 1
+	turn = 1
+	player = -1
 	grid_data = [
 		[0, 0, 0],
 		[0, 0, 0],
 		[0, 0, 0],
 	]
+	get_tree().call_group("circles", "queue_free")
+	get_tree().call_group("crosses", "queue_free")
+	get_tree().paused = false
+	
 	create_marker(Vector2i(0, 0), true)
+	player *= -1
+	$GameOverMenu.hide()
 
 func create_marker(pos: Vector2i, temp: bool = false) -> void:
-	#create a marker node and add as child
 	const panel_pos = Vector2i(1051, 175)
 	const scale = Vector2(0.2, 0.2)
 	var position = grid_pins[pos.y-1][pos.x-1]
 	if player == 1:
-		node = cross_scene.instantiate()
-		node.scale = scale
 		if temp:
+			node = circle_scene.instantiate()
 			node.position = panel_pos
 		else:
+			node = cross_scene.instantiate()
 			node.position = position
+		node.scale = scale
 		add_child(node)
 	else:
-		node = circle_scene.instantiate()
-		node.scale = scale
 		if temp:
+			node = cross_scene.instantiate()
 			node.position = panel_pos
 		else:
+			node = circle_scene.instantiate()
 			node.position = position
+		node.scale = scale
 		add_child(node)
 
-func validate_board() -> void:
+func validate_board() -> bool:
 	# check rows for a win
 	var winner = ""
+	var win = false
 	if abs(grid_data[0][0] + grid_data[0][1] + grid_data[0][2]) == 3:
 		winner = get_winner(grid_data[0][0])
+		win = true
 	if abs(grid_data[1][0] + grid_data[1][1] + grid_data[1][2]) == 3:
 		winner = get_winner(grid_data[1][0])
+		win = true
 	if abs(grid_data[2][0] + grid_data[2][1] + grid_data[2][2]) == 3:
 		winner = get_winner(grid_data[2][0])
+		win = true
 	
 	# check cols for a win
 	if abs(grid_data[0][0] + grid_data[1][0] + grid_data[2][0]) == 3:
 		winner = get_winner(grid_data[0][0])
+		win = true
 	if abs(grid_data[0][1] + grid_data[1][1] + grid_data[2][1]) == 3:
 		winner = get_winner(grid_data[0][1])
+		win = true
 	if abs(grid_data[0][2] + grid_data[1][2] + grid_data[2][2]) == 3:
 		winner = get_winner(grid_data[0][3])
+		win = true
 	
 	# check diags for a win
 	if abs(grid_data[0][0] + grid_data[1][1] + grid_data[2][2]) == 3:
 		winner = get_winner(grid_data[0][0])
+		win = true
 	if abs(grid_data[0][2] + grid_data[1][1] + grid_data[2][0]) == 3:
 		winner = get_winner(grid_data[0][2])
-	$WinnerLabel.text = winner
+		win = true
+	
+	# no one wins
+	if !win && turn >= 9:
+		winner = "Draw"
+		win = true
+	$GameOverMenu/GameOverLabel.text = winner
+	return win
+	
 
 func get_winner(i: int) -> String:
 	if i == 1:
 		return "Crosses Wins!"
 	else:
 		return "Circles Wins!"
+
+func _on_game_over_menu_restart() -> void:
+	new_game()
